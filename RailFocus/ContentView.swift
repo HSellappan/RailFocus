5217d23328b653fc7988982eb88514b0738076af
HSellappan <hasellan@gmail.com>
2026-01-03 15:12:30 -0600

    Initial Commit

---
+++ RailFocus/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  RailFocus
+//
+//  Created by Harold S on 1/3/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ RailFocus/RailFocusApp.swift
@@ -0,0 +1,17 @@
+//
+//  RailFocusApp.swift
+//  RailFocus
+//
+//  Created by Harold S on 1/3/26.
+//
+
+import SwiftUI
+
+@main
+struct RailFocusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
